d940ef505138e44299a9230f6ca81c443376769e
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-10-05 16:38:32 +0200

    Added new assignment

---
+++ Assignment5/script.js
@@ -0,0 +1,37 @@
+/* Part 2 */
+console.log('PART 2')
+
+/* Part 3 */
+console.log('PART 3')
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+/* Part 4 */
+
+/* Part 5 */
+function changeDisplay () {
+
+}
+
+function changeVisibility () {
+
+}
+
+function reset () {
+
+}
+
+/* Part 6 */
+const technologies = [
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'Python',
+    'Java',
+    'AJAX',
+    'JSON',
+    'React',
+    'Angular',
+    'Bootstrap',
+    'Node.js'
+];
