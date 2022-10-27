5dc05df7f57b3af32bd3af4d6c88fa345593fa57
Christio02 <112630243+Christio02@users.noreply.github.com>
2022-10-27 12:08:32 +0200

    Fixed disabled submit button, finished with JS part

--- Assignment7/todo.js
+++ Assignment7/todo.js
@@ -5,8 +5,6 @@ const input = document.querySelector("#input-field");
 
 const list = document.querySelector("#list");
 
-const output = document.querySelector("#output-field")
-
 const completed = document.querySelector('#completed-field');
 
 const checkbox = document.getElementsByClassName('checkbox');
@@ -19,7 +17,12 @@ function addTodo () {
 
     newTodo.style.listStyle = "none"; 
 
-    // functions that creates list and adds the items to an array in the console
+
+   // function that disables submit button when input field is empty
+   submitBtn.disabled = true;
+    
+
+ // functions that creates list and adds the items to an array in the console
     function addNewTask () { 
         newTodo.innerHTML = input.value
 
@@ -34,6 +37,9 @@ function addTodo () {
     }
 
     addNewTask()
+
+
+    
 
     // function that insert a new item on top of the list
     function newItemTop () { 
@@ -52,6 +58,7 @@ function addTodo () {
     }
 
     addCheckbox()
+    
     const checkbox = document.querySelector('.checkbox'); // gets the class for checkbox
 
     // function that adds a line through the list item when the checkbox is clicked and removes it when the checkbox is clicked again
@@ -69,22 +76,14 @@ function addTodo () {
 
     input.value = '' ; // resets the input field
     
-
-    // create a function that updates how many tasks are left and removes tasks when checkbox is clicked
-    function totalTasks () { 
-        output.innerHTML = tasks.length; // updates the output with the length of the array (how many tasks in total)
-    }
-
-    totalTasks(); 
-
-   
-
     function completedTasks () { 
         counter = 0; // counter which counts how many tasks are completed
         completed.innerHTML = counter;
         checkbox.addEventListener('click', function () { // adds eventlistener for clicking checkbox
             if (checkbox.checked) { // if the checkbox is "checked" it should add a number to the counter
                 counter +=1;
+                tasks.pop(input.value); // removes the task from the array
+                console.log(tasks);
                 completed.innerHTML = (`${counter} / ${tasks.length}`) // displays this as a fraction
             }
             else if (checkbox.checked === false) { // if i'ts "unchecked" it should remove one "point" from the counter
@@ -100,31 +99,36 @@ function addTodo () {
     // function that removes the task when a button is clicked, should add a button next to each task
     function removeTask () {
         let removeBtn = document.createElement('button');
+        removeBtn.id = "removeBtn";
         removeBtn.innerHTML = 'x'; // styles the button
         removeBtn.style.marginLeft = '5px';
         newTodo.appendChild(removeBtn);
-       
-        removeBtn.addEventListener('click', function () { // adds eventlistener for clicking removeBtn
+
+        removeBtn.addEventListener('click', function () { // adds eventlistener for clicking the button
             newTodo.remove(); // removes the task
-
-            if (checkbox.checked) { // if the checkbox is checked, and the user removes the task, it should remove one point from the counter
-                tasks.splice(tasks.indexOf(input.value), 1); // removes the task from the array
-                console.log(tasks); // logs the new array
-                totalTasks(); // calls the function to update the output
-                completedTasks(); // calls the function to update the completed tasks
-
-            } else { // else, if the heck
-                tasks.splice(tasks.indexOf(input.value), 1); // removes the task from the array
-                console.log(tasks); // logs the new array
-               // output.innerHTML = tasks.length; // updates the output with the length of the array (how many tasks in total)
-                totalTasks(); // calls the function to update the output
-            }
-            
+            tasks.pop(); // removes the task from the array
+            console.log(tasks);
+            counter -= 1; // removes one point from the counter
+            completed.innerHTML = (`${counter} / ${tasks.length}`) // displays as a fraction
         })
     }
-        removeTask();
+    removeTask();    
 
-
+    // add a function that disables remove button when checkbox is not checked
+    function disableRemove () {
+        removeBtn.disabled = true;
+        checkbox.addEventListener('click', function () {
+            if (checkbox.checked) {
+                removeBtn.disabled = false;
+            }
+            else if (checkbox.checked === false) {
+                removeBtn.disabled = true;
+            }
+        })
+    }
+    disableRemove();
+    
+   
 }
 
 
@@ -132,3 +136,16 @@ submitBtn.addEventListener('click', function(event) { // function that adds an e
     event.preventDefault(); // prevents the page to reload when you submit
     addTodo(); // runs the main function when you submit a task
 });
+
+// function that enables submit button when input field is not empty
+input.addEventListener('input', function () {
+    if (input.value.length === 0 || input.value === '') {
+        submitBtn.style.opacity = "0.5";
+        submitBtn.disabled = true;
+    } else {
+        submitBtn.disabled = false;
+        submitBtn.style.opacity = "1";
+    }
+})
+
+        
